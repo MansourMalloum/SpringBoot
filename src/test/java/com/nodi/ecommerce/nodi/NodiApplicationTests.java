669e68c9e3c6eaad3add51b23ba0ae75d2d04575
mansour.malloum <mansourmalloum@gmail.com>
2019-11-18 15:54:43 +0100

    First Level

---
+++ src/test/java/com/nodi/ecommerce/nodi/NodiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nodi.ecommerce.nodi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NodiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
